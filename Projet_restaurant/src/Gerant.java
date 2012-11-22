d4e9596e4313884fe4b8c62355c8519996880c52
Tristan Frascari <frascari.tristan@gmail.com>
2012-11-22 04:19:07 -0500

    Salut,
    voila le JAVADOC est en place pour comprendre (surtout MoteurBDD)
    
    Bonne nuit

--- Projet_restaurant/src/Gerant.java
+++ Projet_restaurant/src/Gerant.java
@@ -1,20 +1,31 @@
-
+/**
+ * Classe pour les opérations du gérant
+ * @author tristanfrascari
+ * @version 1.0
+ *
+ */
 public class Gerant extends Employe{
 	
 	private void ajoutItemMenu() {
 		
 	}
 	
+	
 	private void modifItemMenu() {
 		
 	}
 	
+	/**
+	 * Permet de supprimer un element du menu
+	 * @param id 
+	 * 				Defini l'id dans la BDD de l'item à supprimer
+	 */
 	private void suppItemMenu(int id) {
 		String query = "DELETE FROM itemMenu WHERE id = " + id;
 		MoteurBdd bdd = new MoteurBdd();
 		
 		 // Afficher dans l'interface quand elle sera OP
-		if (bdd.getResultFromQuery(query) == 0)
+		if (bdd.setResultFromQuery(query) == 0)
 			System.out.println("L'item a bien été supprimé!");
 		else
 			System.out.println("Erreur : Contactez le support technique!");	
--- Projet_restaurant/src/MoteurBdd.java
+++ Projet_restaurant/src/MoteurBdd.java
@@ -4,12 +4,31 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+/**
+ * <b>Classe d'operations de la base de donnees<b>
+ * <p>Elle est caracterisee par les informations suivantes :
+ * <ul>
+ * <li>Un objet de type Connection qui fait le lien avec la BDD</li>
+ * <li>Un objet de type Statement qui s'occupe des requetes</li>
+ * <li>Un objet de type ResultSet qui comprend les resultats de requetes</li>
+ * </ul>
+ * </p>
+ * @author tristanfrascari
+ * @version 1.0
+ */
+
 public class MoteurBdd {
 
 	private Connection con = null;
 	private Statement statement = null;
 	ResultSet result = null;
 	
+	/**
+	 * Constructeur MoteurBdd
+	 * <p>
+	 * A la construction, on initialise les objets Connection et Statement en s'assurant qu'il n'y ai aucune erreurs
+	 * </p>
+	 */
 	public MoteurBdd() {
 		
 		try {
@@ -37,7 +56,15 @@ public class MoteurBdd {
 		}
 	}
 	
-	public int getResultFromQuery(String query) {
+	/**
+	 * Effectue une requete et assigne le resultat de cette requete a l'objet de type ResultSet
+	 * 
+	 * @param query
+	 * 				La requete a executer
+	 * @return		
+	 * 				0 si tout va bien, -1 si une exception a ete levee
+	 */
+	public int setResultFromQuery(String query) {
 		try {
 			result = this.statement.executeQuery(query);
 			return 0;
@@ -46,6 +73,14 @@ public class MoteurBdd {
 			return -1;
 		}
 	}
+	
+	
+	/**
+	 * Ferme toutes les connections
+	 * <p>
+	 * Il est INDISPENSABLE d'appeler cette methode apres avoir utilise l'objet en question
+	 * </p>
+	 */
 	
 	public void closeBdd() {
 		try {
