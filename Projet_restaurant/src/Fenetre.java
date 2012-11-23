ea61fc06e75ff11b156844667185b6201f29d3e2
Patrick <desp23@uqo.ca>
2012-11-23 10:18:50 -0500

    ceci est un message

---
+++ Projet_restaurant/src/Fenetre.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JPanel;
+
+public class Fenetre extends JPanel {
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Projet_restaurant/src/Interface.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+
+public class Interface extends JFrame{
+
+	private Fenetre panneau = new Fenetre();
+	
+	
+	
+}
