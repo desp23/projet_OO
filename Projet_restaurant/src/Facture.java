f6295cb811789bf7b06379f46140042a43abb924
SHL <SHL@SIM>
2012-11-26 19:11:38 -0400

    test

---
+++ Projet_restaurant/src/Facture.java
@@ -0,0 +1,20 @@
+
+public class Facture {
+
+	double montant;
+	int id;
+	public double getMontant() {
+		return montant;
+	}
+	public void setMontant(double montant) {
+		this.montant = montant;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
